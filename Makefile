16e4a7738bd94d39ec2cd7f7f838483c1c9681c3
Samir Talwar <samir@noodlesandwich.com>
2016-06-16 22:15:01 +0200

    Compile the tests as they're run.

--- Makefile
+++ Makefile
@@ -3,12 +3,8 @@ PRODUCTION_NATIVE_FILES = src/Native/Arborist/Framework.js
 TEST_FILES = $(shell find test -name '*.elm')
 
 .PHONY: test
-test: src/run.js build/test.js
-	node src/run.js build/test.js
-
-build/test.js: $(PRODUCTION_FILES) $(PRODUCTION_NATIVE_FILES) $(TEST_FILES)
-	mkdir -p build
-	elm make --warn --yes --output=$@ $(PRODUCTION_FILES) $(TEST_FILES)
+test: src/run.js $(PRODUCTION_FILES) $(PRODUCTION_NATIVE_FILES) $(TEST_FILES)
+	node src/run.js $(TEST_FILES)
 
 node_modules: package.json
 	npm install
--- src/run.js
+++ src/run.js
@@ -1,4 +1,11 @@
-var Elm = require(process.cwd() + '/' + process.argv[2]);
+const child_process = require('child_process');
+const crypto = require('crypto');
+const os = require('os');
+
+let inputFiles = process.argv.slice(2);
+let outputFile = os.tmpdir() + '/' + 'arborist-' + crypto.randomBytes(16).readUInt32LE(0) + '.js';
+child_process.execFileSync('elm', ['make', '--warn', '--yes', '--output', outputFile].concat(inputFiles));
 
 global.window = global;
+let Elm = require(outputFile);
 let app = Elm.Main.worker();
